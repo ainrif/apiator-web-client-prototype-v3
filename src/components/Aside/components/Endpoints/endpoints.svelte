fdf2b6044d7fa21f8fbad650fe4b95580dc4a3da
zelenkoff <zelenkoff.ev@gmail.com>
2020-12-08 23:28:10 +0300

    re-structure endpoints.svelte component

--- src/components/Aside/components/Endpoints/endpoints.svelte
+++ src/components/Aside/components/Endpoints/endpoints.svelte
@@ -1,31 +1,65 @@
 <script lang="ts">
     import { endpoints, selectEndpoint } from '../../../../repository';
 
-    function handleClick(event: any) {
-        if (event) {
-            if (event.target.classList.contains('endpoints-method')) {
-                const name = event.target.dataset.name;
-                const endpointIndex = event.target.dataset.endpointIndex;
+    function handleClick(event: MouseEvent) {
+        const target = event.target as HTMLElement;
 
-                const selectedApi = $endpoints[endpointIndex].apiEndpoints;
+        if (
+            target.classList.contains('endpoints-method')
+        ) {
+            const name = target.dataset.name;
+            const endpointIndex = target.dataset.endpointIndex as unknown as number;
 
-                for (let i = 0; i < selectedApi.length; i++) {
-                    if (selectedApi[i].name === name) {
-                        selectEndpoint(selectedApi[i]);
+            const selectedApi = $endpoints[endpointIndex].apiEndpoints;
 
-                        break;
-                    }
+            for (let i = 0; i < selectedApi.length; i++) {
+                if (selectedApi[i].name === name) {
+                    selectEndpoint(selectedApi[i]);
+
+                    break;
                 }
             }
         }
     }
 </script>
 
+<div class="endpoints">
+    <ul class="endpoints-list" on:click={handleClick}>
+        {#each $endpoints as endpoint, index}
+            <li class="endpoints-item">
+                <span class="endpoints-title">{endpoint.apiPath}</span>
+                <ul class="endpoints-methods">
+                    {#each endpoint.apiEndpoints as entry}
+                        <li
+                            data-name={entry.name}
+                            data-endpoint-index={index}
+                            class="endpoints-method endpoints-method-{entry.method.toLowerCase()}">
+                            {entry.method}   {entry.path}
+                        </li>
+                    {/each}
+                </ul>
+            </li>
+        {/each}
+    </ul>
+</div>
+
 <style>
+    .endpoints {
+        text-overflow: ellipsis;
+        overflow: scroll;
+        white-space: nowrap;
+        width: 100%;
+        box-sizing: border-box;
+    }
+
     ul {
         margin-block-start: 0;
         margin-block-end: 0;
         padding-inline-start: 15px;
+    }
+
+    .path {
+        margin-left: var(--XS-margin);
     }
 
     .endpoints {
@@ -49,6 +83,11 @@
     }
 
     .endpoints-method {
+        display: flex;
+        align-items: center;
+
+        margin-top: var(--XS-margin);
+
         cursor: pointer;
     }
 
@@ -71,24 +110,8 @@
     .endpoints-method-admin {
         color: var(--main-blue);
     }
-</style>
 
-<div class="endpoints">
-    <ul class="endpoints-list" on:click={handleClick}>
-        {#each $endpoints as endpoint, index}
-            <li class="endpoints-item">
-                <span class="endpoints-title">{endpoint.apiPath}</span>
-                <ul class="endpoints-methods">
-                    {#each endpoint.apiEndpoints as entry}
-                        <li
-                            data-name={entry.name}
-                            data-endpoint-index={index}
-                            class="endpoints-method endpoints-method-{entry.method.toLowerCase()}">
-                            {entry.method} {entry.path}
-                        </li>
-                    {/each}
-                </ul>
-            </li>
-        {/each}
-    </ul>
-</div>
+    .endpoints-method-patch {
+        color: var(--main-yellow);
+    }
+</style>
